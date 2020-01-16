dd67f4c89b081b9b75d54e5407de443257dfd9ad
FIC05482 <FIC05482@IC0-W-N09269R.ssg.global>
2020-01-16 18:00:32 +0900

    화면 경로 설정 중...

--- src/main/java/test/cont/TestController.java
+++ src/main/java/test/cont/TestController.java
@@ -1,18 +1,30 @@
 package test.cont;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-	@RequestMapping("/test")
-	public String test() {
-		return "test";
-	}
 	
 	@RequestMapping("/main.do")
-	public String main() {
-		return "main";
+	public String main(Model model) {
+		model.addAttribute("main_jsp", "../main/main1.jsp");
+		return "main/main";
 	}
 	
+	@RequestMapping("/board.do")
+	public String board(Model model) {
+		//model.addAttribute("main_jsp", "../main/board.jsp");
+		return "main/board";
+	}
+	
+	@RequestMapping("/login.do")
+	public String login() {
+		return "main/login";
+	}
+	@RequestMapping("/forgot-password.do")
+	public String forgotPassword() {
+		return "main/forgot-password";
+	}
 }
